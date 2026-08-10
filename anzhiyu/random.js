c6b596d81c81984fa44b1a003d77a5a2eb358fc5
Hui40 <liuchenghui972@gmail.com>
2026-08-10 16:07:26 +0800

    Site updated: 2026-08-10 16:07:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/10/hello-world/"];function toRandomPost(){
+var posts=["2026/08/10/你好/","2026/08/10/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
