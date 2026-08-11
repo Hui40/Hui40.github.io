91e2983b5c58ed0ab0cc7473736582e9259d26fa
Hui40 <liuchenghui972@gmail.com>
2026-08-12 01:13:47 +0800

    Site updated: 2026-08-12 01:13:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["文章/自我介绍/"];function toRandomPost(){
+var posts=["文章/网站导航/","文章/自我介绍/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
